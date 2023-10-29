976d0504bbf13c098f9d1668664647f965fa563a
Yashraj Singh <yashrajsingh977@gmail.com>
2023-10-29 23:23:11 +0530

    Update index.tsx

--- src/components/UserTweets/index.tsx
+++ src/components/UserTweets/index.tsx
@@ -7,7 +7,7 @@ import styles from "../../stylesheets/Feed.module.css";
 const UserTweets: React.FC = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.tab}>Your Tweets</div>
+      <div className={styles.tab}>Your Kwotes</div>
       <div className={styles.tweets}>
         <Tweet type='user' />
       </div>
